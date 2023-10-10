59181a761de03dd7392f497605365c788c14f55d
GriffinBonomo <98484718+GriffinBonomo@users.noreply.github.com>
2023-10-09 23:06:17 -0400

    Updated game resolution

--- globals.js
+++ globals.js
@@ -5,8 +5,8 @@ import StateMachine from "./lib/StateMachine.js";
 export const canvas = document.createElement('canvas');
 export const context = canvas.getContext('2d') || new CanvasRenderingContext2D();
 
-export const CANVAS_WIDTH = 640;
-export const CANVAS_HEIGHT = 360;
+export const CANVAS_WIDTH = 940;
+export const CANVAS_HEIGHT = 480;
 
 export const keys = {};
 export const images = new Images(context);
